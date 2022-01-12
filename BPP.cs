456c4e510dc78d2f6d49e404c63d78eb21203b85
ParlocameonTheDev <parlocameon@outlook.com>
2022-01-11 20:56:45 -0600

    Initial Commit
    Don't dox me this time Penial :)

---
+++ BPP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnboundLib;
+using UnboundLib.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using BepInEx;
+
+namespace BPP
+{
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class BPP
+    {
+        private const string ModId = "com.BPP.rounds.pack";
+        private const string ModName = "BPP";
+        public const string Version = "0.1.0"; // What version are we on (major.minor.patch)?
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            //CustomCard.BuildCard<MyCardName>();
+        }
+
+    }
+}
