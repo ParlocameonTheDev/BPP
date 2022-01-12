9fd208ef6112aff8dd00b9dc7a5fe0d90e0a4e2b
Penial <62630906+PikachuPenial@users.noreply.github.com>
2022-01-11 19:31:51 -0800

    Changed version from 1.0.0 to 0.1.0

--- BPP.cs
+++ BPP.cs
@@ -18,7 +18,7 @@ namespace BPP
     {
         private const string ModId = "com.binarypenialporty.rounds.bpp";
         private const string ModName = "BPP";
-        public const string Version = "1.0.0";
+        public const string Version = "0.1.0";
         public const string ModInitials = "BPP";
 
         public static BPP instance { get; private set; }
